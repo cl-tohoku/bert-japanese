8e6d97272a405da3fe02717ca320275d0d1441c8
Masatoshi Suzuki <sappukei.suzuki@gmail.com>
2019-03-24 22:50:33 +0900

    Initial commit.

---
+++ make_wiki_corpus.py
@@ -0,0 +1,132 @@
+import os
+import re
+import json
+import gzip
+import argparse
+
+import MeCab
+import tensorflow as tf
+
+
+N_DOCS_DEBUG = 1000
+N_DOCS_PER_FILE = 100000
+
+
+class MeCabSentenceSplitter(object):
+    def __init__(self, mecab_dict=None):
+        if mecab_dict is not None:
+            self.mecab_tagger = MeCab.Tagger(f"-d {mecab_dict}")
+        else:
+            self.mecab_tagger = MeCab.Tagger()
+
+    def __call__(self, text):
+        sentences = []
+        start = 0
+        end = 0
+        for line in self.mecab_tagger.parse(text).split("\n"):
+            if line == "EOS":
+                break
+
+            token, morph_info = line.split("\t")
+            end = text.index(token, end) + len(token)
+            if morph_info.startswith("記号,句点,"):
+                sentences.append(text[start:end])
+                start = end
+
+        return sentences
+
+
+def preprocess_text(text, title=None):
+    # remove navigation preceding body text
+    if title:
+        nav_segment = f"> {title}"
+        while nav_segment in text:
+            nav_end = text.index(nav_segment) + len(nav_segment)
+            text = text[nav_end:]
+
+    # squeeze repeated whitespaces
+    text = re.sub(r"\s+", " ", text)
+    return text.strip()
+
+
+def postprocess_text(text):
+    # remove text beginning with a caret
+    text = re.sub(r"\^.*", "", text)
+    return text.strip()
+
+
+def main(args):
+    tf.logging.set_verbosity(tf.logging.INFO)
+    sent_splitter = MeCabSentenceSplitter(args.mecab_dict)
+
+    tf.gfile.MakeDirs(args.output_dir)
+
+    n_processed_docs = 0
+    with tf.gfile.GFile(args.cirrus_file, "rb") as reader, \
+         gzip.GzipFile(fileobj=reader) as cirrus_file:
+        for line in cirrus_file:
+            json_obj = json.loads(line)
+            # ignore non-article pages
+            if json_obj.get("namespace", -1) != 0:
+                continue
+            # ignore articles with few incoming links
+            if json_obj.get("incoming_links", 0) < args.min_inlinks:
+                continue
+            # ignore disambiguation pages
+            templates = json_obj.get("template") or []
+            if "Template:Dmbox" in templates:
+                continue
+
+            title = json_obj["title"]
+            text = json_obj.get("text") or ""
+            text = preprocess_text(text, title)
+            sentences = [postprocess_text(sent) for sent in sent_splitter(text)]
+            # ignore short sentences
+            sentences = [sent for sent in sentences if len(sent) >= args.min_length]
+
+            if sentences:
+                # open a new file at certain intervals
+                if n_processed_docs % N_DOCS_PER_FILE == 0:
+                    file_id = n_processed_docs // N_DOCS_PER_FILE
+                    output_file_path = \
+                        os.path.join(args.output_dir, f"corpus_{file_id:03d}.txt")
+                    output_file = tf.gfile.GFile(output_file_path, "w")
+                    tf.logging.info(f"writing to {output_file_path}")
+
+                # write document to a file
+                for sent in sentences:
+                    assert not "\n" in sent, sent
+                    assert sent, sent
+                    output_file.write(f"{sent}\n")
+
+                output_file.write("\n")
+
+                n_processed_docs += 1
+                if args.debug and n_processed_docs == N_DOCS_DEBUG:
+                    tf.logging.info(f"processed: {n_processed_docs}")
+                    break
+
+                # logging
+                if n_processed_docs % (N_DOCS_PER_FILE // 10) == 0:
+                    tf.logging.info(f"processed: {n_processed_docs}")
+
+    if not args.debug and n_processed_docs % N_DOCS_PER_FILE != 0:
+        tf.logging.info(f"processed: {n_processed_docs}")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("cirrus_file", type=str,
+        help="Wikipedia Cirrussearch dump file (.json.gz)")
+    parser.add_argument("output_dir", type=str,
+        help="output directory to write corpus files")
+    parser.add_argument("--min_inlinks", type=int, default=1,
+        help="limit document size by number of incoming links [1]")
+    parser.add_argument("--min_length", type=int, default=20,
+        help="only extract sentences with N+ characters [20]")
+    parser.add_argument("--mecab_dict", type=str,
+        help="path to MeCab dictionary")
+    parser.add_argument("--debug", action="store_true")
+    args = parser.parse_args()
+
+    main(args)
---
+++ tokenization.py
@@ -0,0 +1,283 @@
+# coding=utf-8
+# Copyright 2018 The Google AI Language Team Authors and The HuggingFace Inc. team.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Tokenization classes."""
+
+import collections
+import unicodedata
+
+import MeCab
+import tensorflow as tf
+
+
+def load_vocab(vocab_file):
+    """Loads a vocabulary file into a dictionary."""
+    vocab = collections.OrderedDict()
+    index = 0
+    with tf.gfile.GFile(vocab_file, "r") as reader:
+        while True:
+            token = reader.readline()
+            if not token:
+                break
+
+            token = token.strip()
+            vocab[token] = index
+            index += 1
+
+    return vocab
+
+
+def whitespace_tokenize(text):
+    """Runs basic whitespace cleaning and splitting on a piece of text."""
+    text = text.strip()
+    if not text:
+        return []
+    tokens = text.split()
+    return tokens
+
+
+class JapaneseBertTokenizer(object):
+    """Runs end-to-end tokenization: punctuation splitting + wordpiece"""
+
+    def __init__(self, vocab_file, do_lower_case=True, mecab_dict=None):
+        """Constructs a BertTokenizer.
+
+        Args:
+            vocab_file: Path to a one-wordpiece-per-line vocabulary file
+            do_lower_case: Whether to lower case the input.
+            mecab_dict: Path to MeCab custom dictionary.
+        """
+
+        self.vocab = load_vocab(vocab_file)
+        self.inv_vocab = {v: k for k, v in self.vocab.items()}
+        self.basic_tokenizer = JapaneseBasicTokenizer(do_lower_case, mecab_dict)
+        self.wordpiece_tokenizer = WordpieceTokenizer(vocab=self.vocab)
+
+    def tokenize(self, text):
+        split_tokens = []
+        for token in self.basic_tokenizer.tokenize(text):
+            for sub_token in self.wordpiece_tokenizer.tokenize(token):
+                split_tokens.append(sub_token)
+
+        return split_tokens
+
+    def convert_tokens_to_ids(self, tokens):
+        """Converts a sequence of wordpiece tokens into ids using the vocab."""
+        ids = []
+        for token in tokens:
+            ids.append(self.vocab[token])
+
+        return ids
+
+    def convert_ids_to_tokens(self, ids):
+        """Converts a sequence of wordpiece ids into tokens using the vocab."""
+        tokens = []
+        for i in ids:
+            tokens.append(self.inv_vocab[i])
+
+        return tokens
+
+
+class JapaneseBasicTokenizer(object):
+    """Runs basic tokenization (such as lower casing) and Japanese tokenization."""
+
+    def __init__(self, do_lower_case=True, mecab_dict=None):
+        """Constructs a BasicTokenizer.
+
+        Args:
+            do_lower_case: Whether to lower case the input.
+            mecab_dict: Path to MeCab custom dictionary.
+        """
+        self.do_lower_case = do_lower_case
+        if mecab_dict is not None:
+            self.mecab_tagger = MeCab.Tagger(f"-d {mecab_dict} -O wakati")
+        else:
+            self.mecab_tagger = MeCab.Tagger("-O wakati")
+
+    def tokenize(self, text):
+        """Tokenizes a piece of text."""
+        text = self._clean_text(text)
+        text = self._tokenize_japanese_words(text)
+        orig_tokens = whitespace_tokenize(text)
+        split_tokens = []
+        for token in orig_tokens:
+            if self.do_lower_case:
+                token = token.lower()
+                token = self._run_strip_accents(token)
+
+            split_tokens.extend(self._run_split_on_punc(token))
+
+        output_tokens = whitespace_tokenize(" ".join(split_tokens))
+        return output_tokens
+
+    def _run_strip_accents(self, text):
+        """Strips accents from a piece of text."""
+        text = unicodedata.normalize("NFD", text)
+        output = []
+        for char in text:
+            cat = unicodedata.category(char)
+            if cat == "Mn":
+                continue
+            output.append(char)
+        return "".join(output)
+
+    def _run_split_on_punc(self, text):
+        """Splits punctuation on a piece of text."""
+        chars = list(text)
+        i = 0
+        start_new_word = True
+        output = []
+        while i < len(chars):
+            char = chars[i]
+            if _is_punctuation(char):
+                output.append([char])
+                start_new_word = True
+            else:
+                if start_new_word:
+                    output.append([])
+                start_new_word = False
+                output[-1].append(char)
+            i += 1
+
+        return ["".join(x) for x in output]
+
+    def _tokenize_japanese_words(self, text):
+        """Split Japanese text into tokens with whitespaces using MeCab."""
+        tokenized_text = self.mecab_tagger.parse(text)
+        tokenized_text = tokenized_text.strip()
+        return tokenized_text
+
+    def _clean_text(self, text):
+        """Performs invalid character removal and whitespace cleanup on text."""
+        output = []
+        for char in text:
+            cp = ord(char)
+            if cp == 0 or cp == 0xfffd or _is_control(char):
+                continue
+            if _is_whitespace(char):
+                output.append(" ")
+            else:
+                output.append(char)
+        return "".join(output)
+
+
+class WordpieceTokenizer(object):
+    """Runs WordPiece tokenization."""
+
+    def __init__(self, vocab, unk_token="[UNK]", max_input_chars_per_word=200):
+        self.vocab = vocab
+        self.unk_token = unk_token
+        self.max_input_chars_per_word = max_input_chars_per_word
+
+    def tokenize(self, text):
+        """Tokenizes a piece of text into its word pieces.
+
+        This uses a greedy longest-match-first algorithm to perform tokenization
+        using the given vocabulary.
+
+        For example:
+            input = "unaffable"
+            output = ["un", "##aff", "##able"]
+
+        Args:
+            text: A single token or whitespace separated tokens. This should have
+                already been passed through `JapaneseBasicTokenizer`.
+
+        Returns:
+            A list of wordpiece tokens.
+        """
+
+        output_tokens = []
+        for token in whitespace_tokenize(text):
+            chars = list(token)
+            if len(chars) > self.max_input_chars_per_word:
+                output_tokens.append(self.unk_token)
+                continue
+
+            is_bad = False
+            start = 0
+            sub_tokens = []
+            while start < len(chars):
+                end = len(chars)
+                cur_substr = None
+                while start < end:
+                    substr = "".join(chars[start:end])
+                    if start > 0:
+                        substr = "##" + substr
+
+                    if substr in self.vocab:
+                        cur_substr = substr
+                        break
+
+                    end -= 1
+
+                if cur_substr is None:
+                    is_bad = True
+                    break
+
+                sub_tokens.append(cur_substr)
+                start = end
+
+            if is_bad:
+                output_tokens.append(self.unk_token)
+            else:
+                output_tokens.extend(sub_tokens)
+
+        return output_tokens
+
+
+def _is_whitespace(char):
+    """Checks whether `char` is a whitespace character."""
+    # \t, \n, and \r are technically contorl characters but we treat them
+    # as whitespace since they are generally considered as such.
+    if char == " " or char == "\t" or char == "\n" or char == "\r":
+        return True
+
+    cat = unicodedata.category(char)
+    if cat == "Zs":
+        return True
+
+    return False
+
+
+def _is_control(char):
+    """Checks whether `char` is a control character."""
+    # These are technically control characters but we count them as whitespace
+    # characters.
+    if char == "\t" or char == "\n" or char == "\r":
+        return False
+
+    cat = unicodedata.category(char)
+    if cat.startswith("C"):
+        return True
+
+    return False
+
+
+def _is_punctuation(char):
+    """Checks whether `char` is a punctuation character."""
+    cp = ord(char)
+    # We treat all non-letter/number ASCII as punctuation.
+    # Characters such as "^", "$", and "`" are not in the Unicode
+    # Punctuation class but we treat them as punctuation anyways, for
+    # consistency.
+    if ((cp >= 33 and cp <= 47) or (cp >= 58 and cp <= 64) or
+            (cp >= 91 and cp <= 96) or (cp >= 123 and cp <= 126)):
+        return True
+
+    cat = unicodedata.category(char)
+    if cat.startswith("P"):
+        return True
+
+    return False
