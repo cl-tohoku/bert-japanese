aa2464587e54a76f528a84a8f0ccdf611877e0f1
Masatoshi Suzuki <sappukei.suzuki@gmail.com>
2019-07-15 06:37:26 +0900

    Revert to use sentencepiece

--- build_vocab.py
+++ build_vocab.py
@@ -1,54 +1,72 @@
 import tempfile
 import os
 import argparse
-from collections import Counter
 
 import sentencepiece
 import tensorflow as tf
-from tensor2tensor.data_generators import text_encoder
 
 from tokenization import MecabBasicTokenizer
 
 
-CONTROL_SYMBOLS = ['[PAD]', '[UNK]', '[CLS]', '[SEP]', '[MASK]']
+CONTROL_SYMBOLS = ['[CLS]', '[SEP]', '[MASK]']
 
 
 def main(args):
     tf.logging.set_verbosity(tf.logging.INFO)
-    token_counts = Counter()
     tokenizer = MecabBasicTokenizer(do_lower_case=args.do_lower_case,
                                     mecab_dict_path=args.mecab_dict_path)
+    with tempfile.TemporaryDirectory() as tempdir:
+        # read input files and write to a temporary file
+        concat_input_file = open(os.path.join(tempdir, 'input.txt'), 'w')
+        for input_file in tf.gfile.Glob(args.input_file):
+            with tf.gfile.GFile(input_file, 'r') as reader:
+                tf.logging.info('Reading {}'.format(input_file))
+                for line in reader:
+                    tokens, _ = tokenizer.tokenize(line.strip('\n'))
+                    print(' '.join(tokens), file=concat_input_file)
 
-    for input_file in tf.gfile.Glob(args.input_file):
-        with tf.gfile.GFile(input_file, 'r') as reader:
-            tf.logging.info('reading {}'.format(input_file))
-            for line in reader:
-                tokens, _ = tokenizer.tokenize(line.strip('\n'))
-                token_counts.update(tokens)
+        # train a SentencePiece model and store the vocabulary file to a temp directory
+        tf.logging.info('Training a SentencePiece model')
+        commands = {
+            'input': concat_input_file.name,
+            'model_prefix': os.path.join(tempdir, 'sp'),
+            'model_type': args.vocab_type,
+            'normalization_rule_name': 'identity',
+            'vocab_size': args.vocab_size,
+            'character_coverage': 1.0,
+            'pad_id': 0,
+            'unk_id': 1,
+            'bos_id': -1,
+            'eos_id': -1,
+            'control_symbols': ','.join(CONTROL_SYMBOLS),
+            'input_sentence_size': args.sentence_size,
+            'shuffle_input_sentence': 'true'
+        }
+        command_line = ' '.join(['--{}={}'.format(k, v) for k, v in commands.items()])
+        sentencepiece.SentencePieceTrainer.Train(command_line)
+        concat_input_file.close()
 
-    tf.logging.info('building WordPiece text encoder')
-    bpe_encoder = text_encoder.SubwordTextEncoder()
-    bpe_encoder.build_from_token_counts(token_counts, args.min_count)
+        # convert SentencePiece vocabulary into WordPiece format that is used in BERT
+        with open(os.path.join(tempdir, 'sp.vocab')) as vocab_file, \
+             tf.gfile.GFile(args.output_file, 'w') as output_file:
+            for line in vocab_file:
+                sp_token, _ = line.rstrip('\n').split('\t')
+                if sp_token == '<pad>':
+                    output_token = '[PAD]'
+                elif sp_token == '<unk>':
+                    output_token = '[UNK]'
+                elif sp_token in CONTROL_SYMBOLS:
+                    output_token = sp_token
+                elif sp_token.startswith('\u2581'):
+                    # e.g. "▁word" -> "word"
+                    output_token = sp_token[1:]
+                elif args.vocab_type == 'bpe':
+                    # e.g. "tion" -> "##tion"
+                    output_token = '##' + sp_token
+                else:
+                    output_token = sp_token
 
-    with tf.gfile.GFile(args.output_file, 'w') as output_file:
-        tf.logging.info('writing vocabulary to {}'.format(output_file))
-
-        for symbol in CONTROL_SYMBOLS:
-            output_file.write(symbol + '\n')
-
-        for subtoken in bpe_encoder.all_subtoken_strings:
-            # ignore pre-defined WordPiece symbols
-            if subtoken in ('<pad>_', '<EOS>_'):
-                continue
-
-            if subtoken.endswith('_'):
-                # e.g. "word_" -> "word"
-                output_token = subtoken[:-1]
-            else:
-                # e.g. "tion" -> "##tion"
-                output_token = '##' + subtoken
-
-            output_file.write(output_token + '\n')
+                output_file.write(output_token + '\n')
 
 
 if __name__ == "__main__":
@@ -57,8 +75,12 @@ if __name__ == "__main__":
         help='Input raw text file (or comma-separated list of files).')
     parser.add_argument('--output_file', type=str, required=True,
         help='Output vocabulary file.')
-    parser.add_argument('--min_count', type=int, default=5,
-        help='Minimum count of subtokens in corpus. [5]')
+    parser.add_argument('--subword_type', choices=('bpe', 'char'), default='bpe',
+        help='Subword type ("bpe" or "char"). [bpe]')
+    parser.add_argument('--vocab_size', type=int, default=32000,
+        help='WordPiece vocabulary size. [32000]')
+    parser.add_argument('--sentence_size', type=int, default=1000000,
+        help='Limit the input sentence size. [1000000]')
     parser.add_argument('--do_lower_case', type=bool, default=False,
         help='Whether to lower case the input text. [False]')
     parser.add_argument('--mecab_dict_path', type=str,
